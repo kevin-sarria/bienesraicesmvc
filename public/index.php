5c1e276f30ee3ca5772ff7180dfa5d8ac7ed852d
Kevin Sarria <reysabueso256@gmail.com>
2022-10-28 22:18:56 -0500

    Proyecto Bienes Raices MVC Terminado

--- public/index.php
+++ public/index.php
@@ -6,7 +6,7 @@ use MVC\Router;
 use Controllers\PropiedadController;
 use Controllers\VendedorController;
 use Controllers\PaginasController;
-
+use Controllers\LoginController;
 
 $router = new Router();
 
@@ -35,6 +35,15 @@ $router->get('/propiedad', [PaginasController::class, 'propiedad']);
 $router->get('/blog', [PaginasController::class, 'blog']);
 $router->get('/entrada', [PaginasController::class, 'entrada']);
 $router->get('/contacto', [PaginasController::class, 'contacto']);
-$router->post('/', [PaginasController::class, 'contacto']);
+$router->post('/contacto', [PaginasController::class, 'contacto']);
+
+
+// Login y Autenticacion
+$router->get('/login', [LoginController::class, 'login']);
+$router->post('/login', [LoginController::class, 'login']);
+$router->get('/logout', [LoginController::class, 'logout']);
+
+
+
 
 $router->comprobarRutas();
